369ee9ab0797d65be4581bf079723a1c11877190
Tioma <arttisch02@gmail.com>
2024-10-12 12:15:36 +0300

    Changes have been made to tasks 2, 3, 4.

--- src/Main.java
+++ src/Main.java
@@ -15,51 +15,55 @@ public class Main {
 
         System.out.println("_______Задание 2________");
 
-        boolean clientDeviceYear = rn.nextBoolean();
+        boolean clientDeviceYear =
+                Integer.parseInt(System.console().readLine("Enter the year of manufacture of your phone model: ")) >= 2015;
+
         if (clientDeviceYear) {
             if (clientOS) {
                 System.out.println("Установите версию приложения для Android по ссылке");
-            } else {
+            }
+            else {
                 System.out.println("Установите версию приложения для iOS по ссылке");
             }
-        } else if (clientOS) {
+        }
+        else {
+            if (clientOS) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+            else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
         }
 
         System.out.println("_______Задание 3________");
 
         int year = Integer.parseInt(System.console().readLine("Enter the year: "));
-        if (year > 1584) {
-            if (year % 100 == 0 && year % 400 != 0) {
-                System.out.println( year + " не високосный год");
-            } else if (year % 4 != 0) {
-                System.out.println( year + " не високосный год");
-            } else {
-                System.out.println( year + " високосный год");
-            }
-        } else {
+        if (year < 1584) {
             System.out.println( year + " Тогда небыло такого понятия как високосный год");
+        }
+        if (year % 100 == 0 && year % 400 != 0 && year % 4 == 0) {
+            System.out.println( year + " не високосный год");
+        } else {
+            System.out.println( year + " високосный год");
         }
 
         System.out.println("_______Задание 4________");
 
         int deliveryDistance = Integer.parseInt(System.console().readLine("Specify the approximate distance from you to the office: "));
-        byte day = 1;
-        if (deliveryDistance < 100) {
-            if (deliveryDistance > 20) {
-                if (deliveryDistance > 60) {
-                    day += 2;
-                    System.out.println("Потребуется дней: " + day);
-                } else {
-                    day += 1;
-                    System.out.println("Потребуется дней: " + day);
-                }
-            } else {
-                System.out.println("Потребуется дней: " + day);
-            }
-        } else {
+        byte deliveryDay = 1;
+        if (deliveryDistance > 20) {
+            deliveryDay++;
+        }
+        if (deliveryDistance > 60) {
+            deliveryDay++;
+        }
+        if (deliveryDistance > 100) {
+            deliveryDay++;
+        }
+        if ( deliveryDay <= 3){
+            System.out.println("Потребуется дней: " + deliveryDay);
+        }
+        else {
             System.out.println("Доставки нет");
         }
 
